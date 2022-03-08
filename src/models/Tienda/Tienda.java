065a3544eab13c7f616d4b6993e7d99aabbdd4de
Augusto <augusto.malaga.t@gmail.com>
2022-03-08 13:32:22 +0100

    Cuerpo inits

--- src/models/Tienda/Tienda.java
+++ src/models/Tienda/Tienda.java
@@ -1,4 +1,17 @@
 package models.Tienda;
 
+import models.ProductoPackage.Categorias.ComponentesPackage.DiscoDuro;
+
 public class Tienda {
+
+    private void initComponentes(){
+        DiscoDuro disco = new DiscoDuro(2,"blabla","m",1048, false, "SSD",100);
+
+    }
+    private void initPC(){
+
+    }
+    private void initPerifericos(){
+
+    }
 }
