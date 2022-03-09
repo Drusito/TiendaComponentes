823415d5f5cf38cac304ad23dfec74cbcb75ac55
Augusto <augusto.malaga.t@gmail.com>
2022-03-09 13:08:57 +0100

    Clases Componentes Terminadas

--- src/models/ProductoPackage/Categorias/ComponentesPackage/PlacaBase.java
+++ src/models/ProductoPackage/Categorias/ComponentesPackage/PlacaBase.java
@@ -1,4 +1,87 @@
 package models.ProductoPackage.Categorias.ComponentesPackage;
 
-public class PlacaBase {
+public class PlacaBase extends Componente{
+    private int numMaxProcesadores;
+    private int puertosRam;
+    private int puertosDiscoDuro;
+    private boolean aceptaSSD;
+    private int puertosUSB;
+    private int puertosJack;
+    boolean ethernet;
+    boolean wifi;
+
+    public PlacaBase(float precio, String descripcion, String marca, int numMaxProcesadores, int puertosRam, int puertosDiscoDuro, boolean aceptaSSD, int puertosUSB, int puertosJack, boolean ethernet, boolean wifi) {
+        super(precio, descripcion, marca);
+        this.numMaxProcesadores = numMaxProcesadores;
+        this.puertosRam = puertosRam;
+        this.puertosDiscoDuro = puertosDiscoDuro;
+        this.aceptaSSD = aceptaSSD;
+        this.puertosUSB = puertosUSB;
+        this.puertosJack = puertosJack;
+        this.ethernet = ethernet;
+        this.wifi = wifi;
+    }
+
+    /**
+     * Getters
+     */
+    public int getNumMaxProcesadores() {
+        return numMaxProcesadores;
+    }
+
+    public int getPuertosRam() {
+        return puertosRam;
+    }
+
+    public int getPuertosDiscoDuro() {
+        return puertosDiscoDuro;
+    }
+
+    public boolean isAceptaSSD() {
+        return aceptaSSD;
+    }
+
+    public int getPuertosUSB() {
+        return puertosUSB;
+    }
+
+    public int getPuertosJack() {
+        return puertosJack;
+    }
+
+    public boolean isEthernet() {
+        return ethernet;
+    }
+
+    public boolean isWifi() {
+        return wifi;
+    }
+    /**
+     * Setters
+     */
+    public void setEthernet(boolean ethernet) {
+        this.ethernet = ethernet;
+    }
+
+    public void setWifi(boolean wifi) {
+        this.wifi = wifi;
+    }
+
+    @Override
+    public String toString() {
+        return "PlacaBase{" +
+                "numMaxProcesadores=" + numMaxProcesadores +
+                ", puertosRam=" + puertosRam +
+                ", puertosDiscoDuro=" + puertosDiscoDuro +
+                ", aceptaSSD=" + aceptaSSD +
+                ", puertosUSB=" + puertosUSB +
+                ", puertosJack=" + puertosJack +
+                ", ethernet=" + ethernet +
+                ", wifi=" + wifi +
+                ", id=" + id +
+                ", precio=" + precio +
+                ", descripcion='" + descripcion + '\'' +
+                ", marca='" + marca + '\'' +
+                '}';
+    }
 }
