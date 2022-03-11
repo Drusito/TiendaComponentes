7984f75cb65c0542ef9eb51057d2aa0a8cc8f3b1
nikit <nikitakuzmenko1107@ghmail.com>
2022-03-11 17:36:06 +0100

    main version 0.3

--- src/Main.java
+++ src/Main.java
@@ -20,7 +20,7 @@ public class Main {
                     menuComprar();
                 }
                 case 2 -> {
-                    System.out.println("Personalizar");
+                    menuPersonalizar();
                 }
                 case 3 -> {
                     mostrarCompras();
@@ -45,8 +45,7 @@ public class Main {
                     menuFinCompra();
                 }
                 case 3 -> {
-                    System.out.println("Tu carrito");
-                    mostrarProductos(CarroCompra.carrito.getCarro());
+                    menuVerCarrito();
                 }
                 case 4 -> {
                     System.out.println("Volviendo atras...");
@@ -54,33 +53,61 @@ public class Main {
             }
         } while (menu != 4);
     }
-
+    
     private static void menuCategorias() {
         int menu = 0;
         do {
             menu = Utilities.leerIntLimites(
-                    "Categorias\n\tEscoge una categoria\n1. Perifericos\n2. PCs\n3. Componentes\n4. Atras", 1, 4);
-            switch (menu) {
-                case 1 -> {
+                "Categorias\n\tEscoge una categoria\n1. Perifericos\n2. PCs\n3. Componentes\n4. Atras", 1, 4);
+                switch (menu) {
+                    case 1 -> {
                     elegirPeriferico();
                 }
                 case 2 -> {
-                    int iProducto = 0;
-                    System.out.println("Esto son todos los perifericos");
-                    mostrarProductos(Tienda.tienda.getPCs());
-                    iProducto = Utilities.leerIntLimites(
-                            "Escribe el indice del pc que quieres comprar o -1 para volver atras", -1,
-                            Tienda.tienda.getPerifericos().size() - 1);
-                    CarroCompra.carrito.añadirAlCarro(Tienda.tienda.getPerifericos().get(iProducto));
+                    elegitPC();
                 }
                 case 3 -> {
-                    // TODO: mostrar Componentes
+                    elegitComponente();
                 }
                 case 4 -> {
                     System.out.println("Volviendo atras...");
                 }
             }
         } while (menu != 4);
+    }
+    
+    private static void menuFinCompra() {
+        if (CarroCompra.carrito.finCompra()) {
+            System.out
+            .println("La compra se ha finalizado el precio total ha sido " + CarroCompra.carrito.precioTotal());
+            System.out.println("Resumen de la compra");
+            System.out.println("Compra numero " + CarroCompra.carrito.getId());
+            mostrarProductos(CarroCompra.getCompras().get(CarroCompra.getCompras().size() - 1).getCarro());
+        } else {
+            System.out.println("No tienes ningun producto en el carrito");
+        }
+    }
+    
+        private static void menuVerCarrito() {
+            if (CarroCompra.carrito.getCarro().size() > 0) {
+                System.out.println("Tu carrito");
+                mostrarProductos(CarroCompra.carrito.getCarro());
+            } else {
+                System.out.println("Tu carrito esta vacio");
+            }
+        }
+
+    private static void elegitPC() {
+        int iProducto = 0;
+        System.out.println("Esto son todos los pc");
+        mostrarProductos(Tienda.tienda.getPCs());
+        iProducto = Utilities.leerIntLimites(
+                "Escribe el indice del portatil o equipo sobre mesa premontado que quieres añadir al carrito o -1 para volver atras (Tambien puedes personalizarte un pc desde el menu principal)",
+                -1,
+                Tienda.tienda.getPCs().size() - 1);
+        if (iProducto != -1) {
+            CarroCompra.carrito.añadirAlCarro(Tienda.tienda.getPCs().get(iProducto));
+        }
     }
 
     private static void elegirPeriferico() {
@@ -88,20 +115,22 @@ public class Main {
         System.out.println("Esto son todos los perifericos");
         mostrarProductos(Tienda.tienda.getPerifericos());
         iProducto = Utilities.leerIntLimites(
-                "Escribe el indice del periferico que quieres comprar o -1 para volver atras", -1,
+                "Escribe el indice del periferico que quieres añadir al carrito o -1 para volver atras", -1,
                 Tienda.tienda.getPerifericos().size() - 1);
-        CarroCompra.carrito.añadirAlCarro(Tienda.tienda.getPerifericos().get(iProducto));
+        if (iProducto != -1) {
+            CarroCompra.carrito.añadirAlCarro(Tienda.tienda.getPerifericos().get(iProducto));
+        }
     }
 
-    private static void menuFinCompra() {
-        if (CarroCompra.carrito.finCompra()) {
-            System.out
-                    .println("La compra se ha finalizado el precio total ha sido " + CarroCompra.carrito.precioTotal());
-            System.out.println("Resumen de la compra");
-            System.out.println("Compra numero " + CarroCompra.carrito.getId());
-            mostrarProductos(CarroCompra.getCompras().get(CarroCompra.getCompras().size() - 1).getCarro());
-        } else {
-            System.out.println("No tienes ningun producto en el carrito");
+    private static void elegitComponente() {
+        int iProducto = 0;
+        System.out.println("Esto son todos los componentes");
+        mostrarProductos(Tienda.tienda.getComponentes());
+        iProducto = Utilities.leerIntLimites(
+                "Escribe el indice del componente que quieres añadir al carrito o -1 para volver atras", -1,
+                Tienda.tienda.getPerifericos().size() - 1);
+        if (iProducto != -1) {
+            CarroCompra.carrito.añadirAlCarro(Tienda.tienda.getPerifericos().get(iProducto));
         }
     }
 
@@ -112,11 +141,15 @@ public class Main {
     }
 
     private static void mostrarCompras() {
-        System.out.println("Estos son tus ultimas compras");
-
-        for (CarroCompra cc : CarroCompra.getCompras()) {
-            System.out.println("Compra numero " + cc.getId());
-            mostrarProductos(cc.getCarro());
+        if (CarroCompra.getCompras().size() > 0) {
+            System.out.println("Estos son tus ultimas compras");
+    
+            for (CarroCompra cc : CarroCompra.getCompras()) {
+                System.out.println("Compra numero " + cc.getId());
+                mostrarProductos(cc.getCarro());
+            }
+        } else {
+            System.out.println("No tienes compras recientes");
         }
     }
 
