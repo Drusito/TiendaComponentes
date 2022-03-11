9ac73b05ad54eee6c92e134e693789e304289fc7
nikit <nikitakuzmenko1107@ghmail.com>
2022-03-11 17:02:23 +0100

    main version 0.2

--- src/Main.java
+++ src/Main.java
@@ -13,8 +13,9 @@ public class Main {
     private static void menuMain() {
         int menu = 0;
         do {
-            menu = Utilities.leerIntLimites("Menu principal\n1. Comprar\n2. Personalizador de pc\n3. Historial de compras\n4. Salir", 1, 4);
-            switch(menu) {
+            menu = Utilities.leerIntLimites(
+                    "Menu principal\n1. Comprar\n2. Personalizador de pc\n3. Historial de compras\n4. Salir", 1, 4);
+            switch (menu) {
                 case 1 -> {
                     menuComprar();
                 }
@@ -28,14 +29,15 @@ public class Main {
                     System.out.println("Saliendo...");
                 }
             }
-        } while(menu != 4);
+        } while (menu != 4);
     }
-    
+
     private static void menuComprar() {
         int menu = 0;
         do {
-            menu = Utilities.leerIntLimites("Menu comprar\n1. Ver categorias\n2. Finalizar compra\n3. Ver carrito\n4. Atras", 1, 4);
-            switch(menu) {
+            menu = Utilities.leerIntLimites(
+                    "Menu comprar\n1. Ver categorias\n2. Finalizar compra\n3. Ver carrito\n4. Atras", 1, 4);
+            switch (menu) {
                 case 1 -> {
                     menuCategorias();
                 }
@@ -50,41 +52,59 @@ public class Main {
                     System.out.println("Volviendo atras...");
                 }
             }
-        } while(menu != 4);
+        } while (menu != 4);
     }
-    
+
     private static void menuCategorias() {
         int menu = 0;
         do {
-            menu = Utilities.leerIntLimites("Categorias\n\tEscoge una categoria\n1. Perifericos\n2. PCs\n3. Componentes\n4. Atras", 1, 4);
-            switch(menu) {
+            menu = Utilities.leerIntLimites(
+                    "Categorias\n\tEscoge una categoria\n1. Perifericos\n2. PCs\n3. Componentes\n4. Atras", 1, 4);
+            switch (menu) {
                 case 1 -> {
-                    mostrarProductos(Tienda.tienda.getPerifericos());
+                    elegirPeriferico();
                 }
                 case 2 -> {
-                    //TODO: mostrar PCs
+                    int iProducto = 0;
+                    System.out.println("Esto son todos los perifericos");
+                    mostrarProductos(Tienda.tienda.getPCs());
+                    iProducto = Utilities.leerIntLimites(
+                            "Escribe el indice del pc que quieres comprar o -1 para volver atras", -1,
+                            Tienda.tienda.getPerifericos().size() - 1);
+                    CarroCompra.carrito.añadirAlCarro(Tienda.tienda.getPerifericos().get(iProducto));
                 }
                 case 3 -> {
-                    //TODO: mostrar Componentes
+                    // TODO: mostrar Componentes
                 }
                 case 4 -> {
                     System.out.println("Volviendo atras...");
                 }
             }
-        } while(menu != 4);
+        } while (menu != 4);
     }
-    
+
+    private static void elegirPeriferico() {
+        int iProducto = 0;
+        System.out.println("Esto son todos los perifericos");
+        mostrarProductos(Tienda.tienda.getPerifericos());
+        iProducto = Utilities.leerIntLimites(
+                "Escribe el indice del periferico que quieres comprar o -1 para volver atras", -1,
+                Tienda.tienda.getPerifericos().size() - 1);
+        CarroCompra.carrito.añadirAlCarro(Tienda.tienda.getPerifericos().get(iProducto));
+    }
+
     private static void menuFinCompra() {
         if (CarroCompra.carrito.finCompra()) {
-            System.out.println("La compra se ha finalizado el precio total ha sido " + CarroCompra.carrito.precioTotal());
+            System.out
+                    .println("La compra se ha finalizado el precio total ha sido " + CarroCompra.carrito.precioTotal());
             System.out.println("Resumen de la compra");
             System.out.println("Compra numero " + CarroCompra.carrito.getId());
-            mostrarProductos(CarroCompra.getCompras().get(CarroCompra.getCompras().size()-1).getCarro());
+            mostrarProductos(CarroCompra.getCompras().get(CarroCompra.getCompras().size() - 1).getCarro());
         } else {
             System.out.println("No tienes ningun producto en el carrito");
         }
     }
-    
+
     private static void mostrarProductos(ArrayList<Producto> ap) {
         for (int i = 0; i < ap.size(); i++) {
             System.out.println(i + ap.get(i).toString());
@@ -93,11 +113,11 @@ public class Main {
 
     private static void mostrarCompras() {
         System.out.println("Estos son tus ultimas compras");
-        
+
         for (CarroCompra cc : CarroCompra.getCompras()) {
             System.out.println("Compra numero " + cc.getId());
             mostrarProductos(cc.getCarro());
         }
     }
-    
+
 }
